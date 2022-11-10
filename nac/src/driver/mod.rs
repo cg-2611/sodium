b7adcb0002becd7bdee9321e80ed72dd955fffcd
Cameron Grant <cameron.grant2611@gmail.com>
2022-11-10 21:41:38 +0000

    update modules

--- nac/src/driver/mod.rs
+++ nac/src/driver/mod.rs
@@ -1,15 +1,3 @@
-use std::env;
+pub mod driver;
 
-use crate::basic::source_file::SourceFile;
-
-pub fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let file = SourceFile::new(&args[1]);
-    compile_file(&file);
-}
-
-fn compile_file(file: &SourceFile) {
-    println!("{}:\n{}", file.path(), file.contents());
-    println!("[nac]: compiled file {}", file.name());
-}
+pub use driver::main;
--- nac/src/util/mod.rs
+++ nac/src/util/mod.rs
@@ -1,1 +1,3 @@
 pub mod file_reader;
+
+pub use file_reader::read_file;
