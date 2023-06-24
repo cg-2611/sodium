0dc63c613d7628ca2c16139db5bc45b463aa7963
Cameron Grant <cameron.grant2611@gmail.com>
2023-06-24 13:54:38 +0100

    update file reader error handling

--- nac/src/util/file_reader.rs
+++ nac/src/util/file_reader.rs
@@ -5,9 +5,9 @@ use crate::errors::{Diagnostic, DiagnosticLevel, Result};
 pub fn read_file(path: &str) -> Result<String> {
     match fs::read_to_string(path) {
         Ok(string) => Ok(string),
-        Err(e) => Err(Diagnostic::without_range(
+        Err(error) => Err(Diagnostic::new(
             DiagnosticLevel::Fatal,
-            format!("could not read contents of file {}: {}", path, e),
+            format!("could not read contents of file {}: {}", path, error),
         )),
     }
 }
